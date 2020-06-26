d692cd626b63d84864e0e831ab1ac833348ead5e
Jared S <jaredjames2020@gmail.com>
2020-06-26 17:53:29 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
-import './App.css'
 
 export class App extends Component {
   render() {
