19fc9027ffd19899ad84f125aa78f155dd652d52
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-06 17:38:13 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import manageRestaurants from './reducers/manageRestaurants';
 
-const store = createStore(manageRestaurants, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(manageRestaurants, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App store={store}/>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
--- src/reducers/manageRestaurants.js
+++ src/reducers/manageRestaurants.js
@@ -1,12 +1,14 @@
-export default function manageRestaurants(state = {
+const initialState = {
   restaurants: []
-}, action) {
+}
+
+export default function manageRestaurants(state = initialState, action) {
   switch (action.type) {
 
     case 'ADD_RESTAURANT':
       return {
         ...state,
-        restaurants: [...state.restaurants, action.restaurant]
+        restaurants: [...state.restaurants, action.payload.restaurant]
       }
 
     default:
